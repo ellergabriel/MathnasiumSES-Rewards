89621266e6242113c015ec5d8cc838171f10fe07
gabe <ellergabriel13@gmail.com>
2024-07-31 17:05:04 -0700

    added form submission, can now login to Radius

--- scraper.py
+++ scraper.py
@@ -1,8 +1,16 @@
 from urllib.request import urlopen
 import re
+import mechanicalsoup
 
-url="https://radius.mathnasium.com"
-page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
-print(html)
+login_url="https://radius.mathnasium.com/Student"
+browser = mechanicalsoup.StatefulBrowser()
+login_page = browser.open(login_url)
+login_html = login_page.soup
+print(login_html)
+browser.select_form()
+#browser.form.print_summary()
+browser["UserName"] = "placeholder"
+browser["Password"] = "placeholder"
+browser.submit_selected()
+browser.launch_browser()
+
