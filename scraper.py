8def7d8a62999ea7032e060941eb115336179d24
gabe <ellergabriel13@gmail.com>
2024-07-31 17:20:47 -0700

    beginning basic GUI implementation using tkinter

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 import re
 import mechanicalsoup
+import tkinter as tk
 
 login_url="https://radius.mathnasium.com/Student"
 browser = mechanicalsoup.StatefulBrowser()
@@ -9,8 +10,9 @@ login_html = login_page.soup
 print(login_html)
 browser.select_form()
 #browser.form.print_summary()
+window = tk.Tk()
 browser["UserName"] = "placeholder"
 browser["Password"] = "placeholder"
-browser.submit_selected()
-browser.launch_browser()
+#response = browser.submit_selected()
+#print(response.text)
 
